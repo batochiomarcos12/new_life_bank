3009f84edbe62773cdf40c5bd1aa8c38c3bc37b5
Marcos Batochio <bmarcospaulooliveira@gmail.com>
2021-10-12 21:49:38 -0300

    Salvando alterações

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from datetime import datetime
 from typing import Dict
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
-@app.get("/ping")
+@app.get("/")
 def  root():
-    return "Pong"
+    return "Welcome to New Life Bank"
+
+@app.get("/health")
+def alive():
+    return {"timestamp": datetime.now()}
+
+if __name__ == "__main__":
+    uvicorn.rum("main:app", host="0.0.0.0.", port=8004, reaload=True)    
